8440701aaefb6da25c8fd207ee0a30abab6b22ba
femincclouds <femin.justin@cognitiveclouds.com>
2022-07-15 16:00:21 +0530

    utils

---
+++ src/types/common.ts
@@ -0,0 +1,20 @@
+export type lotDetailsType = {
+  id: number;
+  reg: string;
+  free: boolean;
+  start: Date;
+};
+
+export type routesType = {
+  Lots: {
+    lots: number;
+  };
+};
+
+export type lotsPropsType = {
+  route: {
+    params: {
+      lots: number;
+    };
+  };
+};
